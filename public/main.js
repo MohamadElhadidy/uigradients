1018f97567991b7b70c3884af764f9f5cfb86fd8
unknown <MohamadElhadidy>
2022-08-17 11:18:15 +0200

    version 1.0.0

---
+++ public/main.js
@@ -0,0 +1,31 @@
+var color1 = document.getElementById('color1');
+var color2 = document.getElementById('color2');
+var main = document.getElementById("gradient");
+var value1 = document.getElementById("value1");
+var value2 = document.getElementById("value2");
+var copied = document.getElementById("copied");
+var button = document.querySelector("button");
+
+
+function changeGradient() {
+  main.style.background =
+    "linear-gradient(to left top," + color1.value + "," + color2.value + ")";
+
+  value1.textContent = color1.value;
+  value2.textContent = color2.value;
+  copied.textContent = "";
+}
+
+function copyGradient() {
+   var background =
+     "linear-gradient(to left top," + color1.value + "," + color2.value + ");";
+   navigator.clipboard.writeText(background);
+   copied.textContent = "Copied";
+}
+
+
+color1.addEventListener("input", changeGradient);
+color2.addEventListener("input", changeGradient);
+
+
+button.addEventListener("click", copyGradient);
